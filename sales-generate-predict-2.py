a62adc4ebdf67ed86134bc685f48da533c317782
kqamar01 <118700648+kqamar01@users.noreply.github.com>
2023-12-21 15:25:21 +0800

    Update sales-generate-predict-2.py

--- sales-generate-predict-2.py
+++ sales-generate-predict-2.py
@@ -4,18 +4,18 @@ import seaborn as sns
 import pickle
 
 
-st.write("# Simple Sales Advertidsing Prediction App")
-st.write("This app predicts the **Sales** type!")
+st.write("# Sales Prediction App")
+st.write("This app predicts sales based on advertising budget.")
 
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 0.7, 296.4, 147.04)
-    Radio = st.sidebar.slider('Radio', 0.0, 49.6, 23.26)
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 30.55)
-    data = {'TV': TV,
-            'Radio': Radio,
-            'Newspaper': Newspaper,}
+    TV = st.sidebar.slider('TV Budget', 0.7, 296.4, 147.04)
+    Radio = st.sidebar.slider('Radio Budget', 0.0, 49.6, 23.26)
+    Newspaper = st.sidebar.slider('Newspaper Budget', 0.3, 114.0, 30.55)
+    data = {'TV Budget': TV,
+            'Radio Budget': Radio,
+            'Newspaper Budget': Newspaper,}
     features = pd.DataFrame(data, index=[0])
     return features
 
